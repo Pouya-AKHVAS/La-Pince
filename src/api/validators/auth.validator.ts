ec94dde2782317dbd9d63812ff1b42bffdadd2c1
MariieVidal <marie.vidal@oclock.school>
2026-04-24 14:49:33 +0200

    Regex mdp & test

--- src/api/validators/auth.validator.ts
+++ src/api/validators/auth.validator.ts
@@ -5,6 +5,10 @@ export const registerSchema = z.object({
   last_name:  z.string().min(1, "Le nom est requis"),
   photo:      z.string().optional(),
   email:      z.email({ pattern: z.regexes.rfc5322Email }), //This is the regex for canonical email addresses according to RFC 5322
-  password:   z.string().min(8, "Le mot de passe doit contenir au moins 8 caractères"),
+  password:   z.string()
+    .min(8, "Le mot de passe doit contenir au moins 8 caractères")
+    .regex(/[A-Z]/, "Le mot de passe doit contenir au moins une lettre majuscule")
+    .regex(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre")
+    .regex(/[@$!%*?&]/, "Le mot de passe doit contenir au moins un caractère spécial (@, $, !, %, *, ?, &)")
 });
 
