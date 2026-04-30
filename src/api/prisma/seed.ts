a5c135748c0a0ae6fcb20aa0086d7204131cb589
cathychereau <cathy.chereau@oclock.school>
2026-04-30 23:15:53 +0200

    fix categories not possible to create update delete , and more categories of expense, income

--- src/api/prisma/seed.ts
+++ src/api/prisma/seed.ts
@@ -9,58 +9,50 @@ const prisma = new PrismaClient({ adapter });
 async function main() {
   console.log('🌱 Début du seeding catégories...');
 
-  // création user pour créer une catégorie
-  const user = await prisma.user.upsert({
-    where: { email: 'test@test.com' },
-    update: {},
-    create: {
-      email: 'test@test.com',
-      password: 'test',
-      first_name: 'Test',
-      last_name: 'User',
-    },
-  });
-
-  const userId = user.id;
-  
   
   const defaultCategories = [
-    { name: 'Salaire', type: CategoryType.INCOME, color: '#10B981', icon: 'Banknote' },
-    { name: 'Alimentation', type: CategoryType.EXPENSE, color: '#F59E0B', icon: 'ShoppingCart' },
-    { name: 'Logement', type: CategoryType.EXPENSE, color: '#6366F1', icon: 'Home' }
-  ];
+    // INCOME
+  
+  { name: 'Salaire', type: CategoryType.INCOME, color: '#10B981', icon: 'Banknote' },
+  { name: 'Freelance', type: CategoryType.INCOME, color: '#22C55E', icon: 'Laptop' },
+  { name: 'Investissements', type: CategoryType.INCOME, color: '#16A34A', icon: 'TrendingUp' },
+  { name: 'Aides / Allocations', type: CategoryType.INCOME, color: '#059669', icon: 'HandCoins' },
+  { name: 'Autres revenus', type: CategoryType.INCOME, color: '#34D399', icon: 'PlusCircle' },
+
+
+  // EXPENSE
+ 
+  { name: 'Alimentation', type: CategoryType.EXPENSE, color: '#F59E0B', icon: 'ShoppingCart' },
+  { name: 'Logement', type: CategoryType.EXPENSE, color: '#6366F1', icon: 'Home' },
+  { name: 'Transport', type: CategoryType.EXPENSE, color: '#EF4444', icon: 'Car' },
+  { name: 'Factures', type: CategoryType.EXPENSE, color: '#3B82F6', icon: 'Receipt' },
+  { name: 'Restaurants', type: CategoryType.EXPENSE, color: '#FB923C', icon: 'UtensilsCrossed' },
+  { name: 'Shopping', type: CategoryType.EXPENSE, color: '#EC4899', icon: 'ShoppingBag' },
+  { name: 'Abonnements', type: CategoryType.EXPENSE, color: '#8B5CF6', icon: 'CreditCard' },
+  { name: 'Jeux vidéo', type: CategoryType.EXPENSE, color: '#A855F7', icon: 'Gamepad2' },
+  { name: 'Sport', type: CategoryType.EXPENSE, color: '#10B981', icon: 'Dumbbell' },
+  { name: 'Voyages', type: CategoryType.EXPENSE, color: '#06B6D4', icon: 'Plane' },
+  { name: 'Santé', type: CategoryType.EXPENSE, color: '#14B8A6', icon: 'HeartPulse' },
+  { name: 'Cadeaux', type: CategoryType.EXPENSE, color: '#F43F5E', icon: 'Gift' },
+  { name: 'Autres', type: CategoryType.EXPENSE, color: '#9CA3AF', icon: 'MoreHorizontal' },
+]
+  
+
       
-    for (const cat of defaultCategories) {
-    
-    await prisma.category.upsert({
-      where: { 
-      name_id_user: {
-      name: cat.name,  
-      id_user: userId,
-      },
-    },
-
-    update: {},
-
-    create: {
-        name: cat.name,
-        type: cat.type,
-        color: cat.color,
-        icon: cat.icon,
-        is_default: true,
-        id_user: userId,
-      },
-    });
-  }
+    await prisma.category.createMany({
+  data: defaultCategories,
+  skipDuplicates: true,
+});
 
   console.log('✅ Seeding terminé');
 }
-
 main()
+// si c'est un echec, on affiche l'erreur et on arrête tout
   .catch((e) => {
     console.error('❌ Seed error:', e);
     process.exit(1);
   })
+  //ferme la connexion dans tout les cas
   .finally(async () => {
     await prisma.$disconnect();
   });
