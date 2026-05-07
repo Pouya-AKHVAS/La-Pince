85138d059f232c44f00d7934c559512f8d611568
Julien Mimouni <julienmimouni1@gmail.com>
2026-05-07 07:44:16 -0400

    ajout du L majuscule

--- src/client/src/components/Layout/PrivateLayout.tsx
+++ src/client/src/components/Layout/PrivateLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import LogoutButton from "../logout/LogoutButton";
+import LogoutButton from "../Logout/LogoutButton";
 import { useAuth } from "../../context/AuthContext";
 
 export default function PrivateLayout() {
